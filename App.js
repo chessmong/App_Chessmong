aaca08a6be4e9bae7a287b2c08b27f0c4d5de309
MEGUMMY1 <cys10902@nate.com>
2024-12-29 03:04:23 +0900

    Fix: webview option 수정

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import { WebView } from "react-native-webview";
+import React, { useRef } from "react";
 
 export default function App() {
+  const webviewRef = useRef(null);
+
+  const reloadPage = () => {
+    if (webviewRef.current) {
+      webviewRef.current.reload();
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: "https://chessmong.com" }} style={{ flex: 1 }} />
+      <WebView
+        ref={webviewRef}
+        source={{ uri: "https://chessmong.com" }}
+        style={{ flex: 1 }}
+        originWhitelist={["*"]}
+        cacheEnabled={false}
+      />
+      <Button title="Reload Page" onPress={reloadPage} />
       <StatusBar style="auto" />
     </View>
   );
